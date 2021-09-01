fb60a884b9c0f35d0c2aa4f16f8f856bcbb19315
Михаил Виноградчий <saltiforvdeveloper@gmail.com>
2021-09-01 10:39:55 +0300

    fix email validation and png

--- js/validation.js
+++ js/validation.js
@@ -5,6 +5,7 @@ const email = document.querySelector('.form__email')
 const phone = document.querySelector('.form__number')
 const name = document.querySelector('.form__name')
 const errorMessage = document.querySelector('.employer__form')
+const checkbox = document.querySelector('.form__checkbox')
 const fields = document.querySelectorAll('.form__input')
 
 name.addEventListener('input', (e) => {
@@ -37,18 +38,25 @@ phone.addEventListener('input', (e) => {
 
 })
 form.addEventListener('input', (e) => {
-    for (let i = 0; i < fields.length; i++) {
-        if (fields[i].value) {
+        if (name.value && checkbox.checked && phone.value && email.value) {
             document.querySelector('.form__btn').classList.remove('disabled')
+        } else {
+            document.querySelector('.form__btn').classList.add('disabled')
         }
-    }
 })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('submit',)
+
+    let domain = email.value.slice((email.value.indexOf("@") + 1), email.value.length)
+
+    if (!emails.includes(String(domain).toLowerCase()) && !(email.value.indexOf("@") + 1)) {
+        errorMessage.classList.add('show-error')
+    } else {
+        errorMessage.classList.remove('show-error')
+    }
     for (let i = 0; i < fields.length; i++) {
-        if (!fields[i].value) {
+        if (!fields[i].value && checkbox.checked) {
             document.querySelector('.form__btn').classList.add('disabled')
         }
     }
